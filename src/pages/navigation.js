0b25a11f5820bcf7597cad4e83445a46346400a4
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-01-26 15:35:54 +0100

    format code

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -1,28 +1,55 @@
-import { Navigation, ImgLogo, Menu, MenuList, Span, Hamburger, CloseIcon } from "../style/navigation/styled";
-import logo from "../assets/shared/logo.svg";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  Navigation,
+  ImgLogo,
+  Menu,
+  MenuList,
+  Span,
+  Hamburger,
+  CloseIcon,
+} from "../style/navigation/styled";
+import logo from "../assets/shared/logo.svg";
 import hamburger from "../assets/shared/icon-hamburger.svg";
-import { useState} from "react";
 import closeIcon from "../assets/shared/icon-close.svg";
 
-export const Nav = () => {
+export default function Nav() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-      <>
-    <Navigation>
-            <ImgLogo src={logo} alt="Space logo" />
+    <>
+      <Navigation>
+        <ImgLogo src={logo} alt="Space logo" />
         <Menu open={navOpen}>
-          <CloseIcon src={closeIcon}></CloseIcon>
-                <MenuList className="menu-list">
-                    <Link to="/space-tourism/" className="menu-item"><Span><strong>00</strong> HOME</Span></Link>
-                    <Link to="/destination/" className="menu-item"><Span><strong>01</strong> DESTINATION</Span></Link>
-                    <Link to="/crew/" className="menu-item"><Span><strong>02</strong> CREW</Span></Link>
-                    <Link to="/technology/" className="menu-item"><Span><strong>03</strong> TECHNOLOGY</Span></Link>
-                </MenuList>
-                </Menu>
+          <CloseIcon src={closeIcon} />
+          <MenuList className="menu-list">
+            <Link to="/space-tourism/" className="menu-item">
+              <Span>
+                <strong>00</strong> HOME
+              </Span>
+            </Link>
+            <Link to="/destination/" className="menu-item">
+              <Span>
+                <strong>01</strong> DESTINATION
+              </Span>
+            </Link>
+            <Link to="/crew/" className="menu-item">
+              <Span>
+                <strong>02</strong> CREW
+              </Span>
+            </Link>
+            <Link to="/technology/" className="menu-item">
+              <Span>
+                <strong>03</strong> TECHNOLOGY
+              </Span>
+            </Link>
+          </MenuList>
+        </Menu>
       </Navigation>
-      <Hamburger src={navOpen ? closeIcon : hamburger} onClick={() => setNavOpen(c => !c)}></Hamburger>
-      </>
-    )
+      <Hamburger
+        src={navOpen ? closeIcon : hamburger}
+        onClick={() => setNavOpen((c) => !c)}
+      />
+    </>
+  );
 }
