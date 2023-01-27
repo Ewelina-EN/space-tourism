c409647010a3599bf24ff948841fa9b4e38f3d5e
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-01-27 11:56:04 +0100

    Changing the website URL

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import Homepage from "./pages/homepage";
-import { Nav } from "./pages/navigation";
-import GlobalStyle from "./style/globalStyle";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage";
+import Nav from "./pages/navigation";
+import GlobalStyle from "./style/globalStyle";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Nav/>
-        <GlobalStyle />
-          <Routes>
-           <Route exact path='/space-tourism-website-react-NEW' element={<Homepage />}></Route>
-          </Routes>
-      </Router>   
-    </>
+    <Router>
+      <Nav />
+      <GlobalStyle />
+      <Routes>
+        <Route exact path="/space-tourism" element={<Homepage />} />
+      </Routes>
+    </Router>
   );
 }
 
