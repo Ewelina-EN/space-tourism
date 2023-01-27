c3459c0c130520fed4f7bd9890d981fa98d385c2
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-01-27 21:10:37 +0100

    refactor code in navigation&add fonts

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import {
   Navigation,
   ImgLogo,
   Menu,
   MenuList,
-  Span,
+  ListItem,
+  Number,
   Hamburger,
-  CloseIcon,
 } from "../style/navigation/styled";
 import logo from "../assets/shared/logo.svg";
-import hamburger from "../assets/shared/icon-hamburger.svg";
+import hamburgerIcon from "../assets/shared/icon-hamburger.svg";
 import closeIcon from "../assets/shared/icon-close.svg";
 
 export default function Nav() {
@@ -21,34 +20,26 @@ export default function Nav() {
       <Navigation>
         <ImgLogo src={logo} alt="Space logo" />
         <Menu open={navOpen}>
-          <CloseIcon src={closeIcon} />
-          <MenuList className="menu-list">
-            <Link to="/space-tourism/" className="menu-item">
-              <Span>
-                <strong>00</strong> HOME
-              </Span>
-            </Link>
-            <Link to="/destination/" className="menu-item">
-              <Span>
-                <strong>01</strong> DESTINATION
-              </Span>
-            </Link>
-            <Link to="/crew/" className="menu-item">
-              <Span>
-                <strong>02</strong> CREW
-              </Span>
-            </Link>
-            <Link to="/technology/" className="menu-item">
-              <Span>
-                <strong>03</strong> TECHNOLOGY
-              </Span>
-            </Link>
+          <MenuList>
+            <ListItem to="/space-tourism/">
+              <Number>00</Number> HOME
+            </ListItem>
+            <ListItem to="/destination/">
+              <Number>01</Number> DESTINATION
+            </ListItem>
+            <ListItem to="/crew/">
+              <Number>02</Number> CREW
+            </ListItem>
+            <ListItem to="/technology/">
+              <Number>03</Number> TECHNOLOGY
+            </ListItem>
           </MenuList>
         </Menu>
       </Navigation>
       <Hamburger
-        src={navOpen ? closeIcon : hamburger}
-        onClick={() => setNavOpen((c) => !c)}
+        src={navOpen ? closeIcon : hamburgerIcon}
+        onClick={() => setNavOpen(!navOpen)}
+        alt="menu icon"
       />
     </>
   );
